0450e64521e5df9f34412c9b840ea050320da5e3
benyarb <ben@benyarb.com>
2023-04-23 02:39:59 +0000

    add fetch

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+async function test() {
+  let url = 'https://www.kyleyarb.com/stuff.html';
+  let response = await fetch(url);
+
+  let text = await response.text(); // read response body as text
+
+  // alert(text);
+  var p = document.getElementById('addStuffHere');
+  p.innerHTML += text;
+}
